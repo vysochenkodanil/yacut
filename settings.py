4c76006c651698875be8efca1f1e5deedfab77e4
danil <vysochenkodanil@gmail.com>
2025-08-09 19:41:00 +0300

    поправил pep8

--- settings.py
+++ settings.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
+
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'fallback_secret_key')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///db.sqlite3')
