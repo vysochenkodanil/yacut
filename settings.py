4a18cd910202c312404795f71dffe5602b734520
danil <vysochenkodanil@gmail.com>
2025-08-10 16:33:26 +0300

    добавлено описание проекта

--- settings.py
+++ settings.py
@@ -5,6 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 MAX_LEIGHT = 16
 
+
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'fallback_secret_key')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///db.sqlite3')
